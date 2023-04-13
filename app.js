e61eaec6a384bb3afaff2c910f5941d7e13eed58
Victoria Pham <victoriapham52@gmail.com>
2023-04-13 09:40:53 -0700

    Exercise commit

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-console.log("Hello World!\n==========\n");
+console.log();
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & 2
+// const firstName;
+// let lastName;
+// var age;
 
-// Exercise 2
+const firstName = "Victoria";
+let lastName = "Pham";
+var age = 31;
+console.log(`Hi my name is ${firstName} ${lastName}.`)
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log(`${language} was created in ${createdYear}.`)
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
